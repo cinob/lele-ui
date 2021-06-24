5dff4772585ed32375039d5a3a65c45590ed40c8
cinob <cinob@foxmail.com>
2021-06-24 11:09:39 +0800

    radio component

--- lib/radio/src/radio.vue
+++ lib/radio/src/radio.vue
@@ -1,7 +1,38 @@
 <template>
-  <label class="le-radio">
-    <input type="radio">
-    <span>
+  <label class="le-radio"
+    :class="[
+      border && radioSize ? 'le-radio-' + radioSize : '',
+      {
+        'is-focus': focus,
+        'is-checked': isEqual,
+        'is-bordered': border,
+        'is-disabled': isDisabled
+      }
+    ]"
+    :tabindex="tabIndex"
+  >
+    <span class="le-radio-input"
+      :class="{
+        'is-checked': isEqual,
+        'is-disabled': isDisabled
+      }"
+    >
+      <span class="le-radio-inner"></span>
+      <input
+        ref='radio'
+        class="le-radio-original"
+        type="radio"
+        aria-hidden='true'
+        :value="label"
+        v-model="model"
+        @focus='focus = true'
+        @blur='focus = false'
+        @change="handleChange"
+        :disabled="isDisabled"
+        tabindex='-1'
+      >
+    </span>
+    <span class='le-radio-label'>
       <slot />
       <template v-if="!$slots.default">{{ label }}</template>
     </span>
@@ -9,14 +40,145 @@
 </template>
 
 <script setup>
-import { defineProps } from "vue"
-defineProps({
+import { computed, defineEmit, defineProps, ref } from "vue"
+
+const prop = defineProps({
   label: {},
-  value: {}
+  modelValue: {},
+  disabled: Boolean,
+  name: String,
+  border: Boolean,
+  size: String
 })
+
+const emit = defineEmit([
+  'update:modelValue'
+])
+
+const radio = ref()
+
+const model = computed({
+  get () {
+    return prop.modelValue
+  },
+  set (val) {
+    emit('update:modelValue', val)
+    radio.value && (radio.value.checked = isEqual.value)
+  }
+})
+
+const isEqual = computed(() => {
+  return (model.value|0) === (prop.label|0)
+})
+
+const isDisabled = computed(() => {
+  return prop.disabled
+})
+
+const tabIndex = computed(() => {
+  return isDisabled.value ? -1 : 0
+})
+
+
+const focus = ref(false)
+
+function handleChange () {
+  emit('change', model.value)
+}
+
 </script>
 
 <style lang="stylus" scoped>
 @import '../../../src/styles/global.styl'
-
+@import '../../../src/styles/var.styl'
+.le-radio
+  line-height 1
+  cursor pointer
+  display inline-block
+  white-space nowrap
+  font-size $font-size-base
+  margin-right 30px
+  &:last-child
+    margin-right 0
+  .le-radio-input
+    white-space nowrap
+    cursor pointer
+    outline none
+    display inline-block
+    line-height 1
+    position relative
+    vertical-align middle
+    /&.is-checked
+      .le-radio-inner
+        border-color $radio-checked-input-border-color
+        background $radio-checked-icon-color
+        &::after
+          transform translate(-50%, -50%) scale(1)
+      & + .le-radio-label
+        color $radio-checked-font-color
+    /&.is-disabled
+      .le-radio-inner
+        background-color $radio-disabled-input-fill
+        border-color $radio-disabled-input-border-color
+        cursor not-allowed
+        &::after
+          cursor not-allowed
+          background-color $radio-disabled-icon-color
+        & + .le-radio-label
+          cursor not-allowed
+      &.is-checked
+        .le-radio-inner
+          background-color $radio-disabled-checked-input-fill
+          border-color $radio-disabled-checked-input-border-color
+          &::after
+            background-color $radio-disabled-checked-icon-color
+      & + span.le-radio-label
+        color $color-text-placeholder
+        cursor not-allowed
+    .le-radio-inner
+      border $radio-input-border
+      border-radius $radio-input-border-radius
+      width $radio-input-width
+      height $radio-input-height
+      background-color $radio-input-background-color
+      position relative
+      cursor pointer
+      display inline-block
+      box-sizing border-box
+      &::after
+        square(4px)
+        border-radius @border-radius
+        background-color $color-white
+        content ""
+        position absolute
+        left 50%
+        top 50%
+        transform translate(-50%, -50%) scale(0)
+        transition transform .15s ease-in
+    .le-radio-original
+      opacity 0
+      outline none
+      position absolute
+      z-index -1
+  .le-radio-label
+    font-size $radio-font-size
+    padding-left 10px
+  /&.is-bordered
+    padding $radio-bordered-padding
+    border-radius $border-radius-base
+    border $border-base
+    box-sizing border-box
+    height $radio-bordered-height
+    &.is-checked
+      border-color $color-primary
+    &.is-disabled
+      cursor not-allowed
+      border-color $border-color-lighter
+    & + .el-radio.is-bordered
+      margin-left 10px
+  .le-radio-medium
+    &.is-bordered
+      padding $radio-bordered-medium-padding
+      border-radius $button-medium-border-radius
+      height $radio-bordered-medium-height
 </style>
--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,9 @@
   <HelloWorld msg="Hello Vue 3 + Vite" />
   <Test />
   <BackToTop />
-  
-  <h2>按钮样式测试</h2>
-  <LeButton circle size='small'>普通圆形small大小按钮</LeButton>
-  <p>按钮组演示</p>
-  <LeButtonGroup>
-    <LeButton round>默认圆角按钮</LeButton>
-    <LeButton size='medium' type='primary'>medium大小的primary样式按钮</LeButton>
-    <LeButton circle size='small' type='danger'>圆形</LeButton>
-    <LeButton size='mini' type='danger' plain round>mini大小danger样式圆角朴素按钮</LeButton>
+  <LeButtonGroup style='margin: 0 auto;width: fit-content'>
+    <LeButton round type='primary'>按钮1</LeButton>
+    <LeButton type='danger' disabled>按钮2</LeButton>
   </LeButtonGroup>
   <LeBackToTop />
   <LeIcon name='loading'/>
@@ -18,14 +12,17 @@
     <LeLink href='https://www.cinob.cn' target='_blank' type="primary" icon="le-icon-edit">编辑</LeLink>
     <LeLink href='https://www.cinob.cn' type="warning" disabled afterIcon="le-icon-loading">编辑</LeLink>
   </p>
-  <LeRadio v-model="radio" label='1'>单选1</LeRadio>
-  <LeRadio v-model="radio" label='2'>单选2</LeRadio>
+  <LeRadio v-model="radio" border @test='test' label='1'>单选1</LeRadio>
+  <LeRadio v-model="radio" label='2' disabled>单选2</LeRadio>
 
 </template>
 
 <script setup>
 import { ref } from 'vue'
-const radio = ref(null)
+const radio = ref('1')
+function test () {
+  console.log('test');
+}
 </script>
 
 <style>
